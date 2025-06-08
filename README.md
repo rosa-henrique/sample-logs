84ba66d192dd54066cd8ddbaf1b3f351dbf4e229
henrique <rosa.henrique@outlook.com.br>
2025-06-08 17:29:13 -0300

    feat: added sink serilog to elastic directly

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-docker compose -f docker-compose.base.yml -f .\logstash\docker-compose.logstash.yml up -d docker exec -it kafka kafka-console-producer --bootstrap-server localhost:9092 --topic logs_logstash
+docker compose -f docker-compose.base.yml -f .\logstash\docker-compose.logstash.yml up -d 
+docker exec -it kafka kafka-console-producer --bootstrap-server localhost:9092 --topic logs_logstash
 
 docker compose -f docker-compose.kafka-connector.yml up -d
 ocker exec kafka kafka-topics --create --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 --topic logs_connector
---
+++ src/SampleLogs.SerilogElastic/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using SampleLogs.SerilogElastic;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
+
+app.UseMiddleware<RequestContextLoggingMiddleware>();
+
+app.MapPost("", ([FromBody] Request request, ILogger<Program> logger) =>
+{
+    logger.LogInformation("Request {@Request}", request);
+
+    if (request.Title.Equals("ex"))
+    {
+        logger.LogError("Request exception {@Request}", request);
+        throw new ArgumentException("test");
+    }
+    
+    return Results.Ok(request);
+});
+
+app.Run();
+
+public record Request(string Title);
